5955fa187b29193b5e865d91cdcf8018ff306b4b
nick90903 <nhatchew03@gmail.com>
2022-04-07 21:37:01 -0500

    Whole base site finished
    Picture scales correctly and right pane has all sets of items

---
+++ script.js
@@ -0,0 +1,19 @@
+let one = document.getElementById('password').value
+let two = document.getElementById('confirm-password').value
+let three = document.getElementById('message').value;
+
+if(one == null | two == null | three == null) {
+    console.log(`one ${one} two ${two} three ${three}`);
+}
+
+let check = function() {
+    if (document.getElementById('password').value ==
+      document.getElementById('confirm-password').value) {
+      document.getElementById('message').style.color = 'green';
+      document.getElementById('message').innerHTML = '';
+    } else {
+      document.getElementById('message').style.color = 'red';
+      document.getElementById('message').style.fontSize = '10px';
+      document.getElementById('message').innerHTML = '*Passwords do not match';
+    }
+  }
